7936a6a816af22c8004f8a1381331538962a9164
czh789dv <49721043+czh789dv@users.noreply.github.com>
2019-07-30 22:18:19 +0800

    11

--- czh-reader-pro/src/App.vue
+++ czh-reader-pro/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-        <span id='sss'>sadasdasd</span>
+      <span id='sss'>sadasdasd</span>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -10,7 +10,34 @@
 </template>
 
 <script>
-export default {}
+import {
+  mapGetters
+} from 'vuex'
+const getters = {
+  a: () => 1,
+  b: () => 2
+}
+
+function fn(keys) {
+  const data = {}
+  keys.forEach(key => {
+    if (getters.hasOwnProperty(key)) {
+      data[key] = getters[key]
+    }
+  })
+  return data
+}
+export default {
+  computed: {
+    ...mapGetters(['test']),
+    ...fn(['a', 'b', 'c'])
+  },
+  mounted() {
+    this.$store.dispatch('setTest', 9).then(() => {
+      console.log(this.$store.state.book.test, this.test, this.a, this.b)
+    })
+  }
+}
 document.addEventListener('DOMContentLoaded', () => {
   const html = document.querySelector('html')
   let fontSize = window.innerWidth / 10
@@ -20,9 +47,12 @@ document.addEventListener('DOMContentLoaded', () => {
 </script>
 
 <style lang="scss" scoped>
-@import "./assets/styles/global.scss" ;
-#sss{
-    color:orange;
-    font-size: px2rem(20)
+@import "./assets/styles/global.scss";
+
+#sss {
+  color: orange;
+  font-size: px2rem(20);
+  font-family: daysOne;
+
 }
 </style>
