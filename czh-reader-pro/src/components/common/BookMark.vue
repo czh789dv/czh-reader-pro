ff6637f9837d801ef41b27e074523d3122957c12
czh789dv <49721043+czh789dv@users.noreply.github.com>
2019-08-13 18:10:20 +0800

    书签下拉改变

--- czh-reader-pro/src/components/common/BookMark.vue
+++ czh-reader-pro/src/components/common/BookMark.vue
@@ -1,21 +1,35 @@
 <template>
-  <div class="bookmark" :style="style">
+  <div class="bookmark" :style="style" ref="bookmark">
 
   </div>
 </template>
 
 <script>
+import {
+  px2rem
+} from '../../utils/utils'
 export default {
   props: {
     width: Number,
     height: Number,
     color: String
   },
+  methods: {
+    refresh() {
+      console.log(this.height, this.width)
+      if (this.height && this.width) {
+        this.$refs.bookmark.style.borderWidth = `${px2rem(this.height - 5)}rem ${px2rem(this.width / 2)}rem ${px2rem(5)}rem ${px2rem(this.width / 2)}rem`
+      }
+    }
+  },
+  mounted() {
+    this.refresh()
+  },
   computed: {
     style() {
       if (this.color) {
         return {
-          borderColor: `${this.color} ${this.color} transparent ${this.color};`
+          borderColor: `${this.color} ${this.color} transparent ${this.color}`
         }
       } else {
         return {}
@@ -39,6 +53,6 @@ export default {
   height: 0;
   border-width: px2rem(40) px2rem(10) px2rem(10) px2rem(10);
   border-style: solid;
-  border-color: wheat wheat transparent wheat;
+  border-color: white white transparent white;
 }
 </style>
--- czh-reader-pro/src/components/ebook/EbookBookmark.vue
+++ czh-reader-pro/src/components/ebook/EbookBookmark.vue
@@ -1,26 +1,56 @@
 <template>
-  <div class="ebook-bookmark">
+  <div class="ebook-bookmark" ref="bookmark">
     <div class="ebook-boormark-text-wrapper">
       <div class="ebook-bookmark-dowm-wrapper">
-        <span class="iconfont icon-down"></span>
-        <div class="ebook-bookmark-text">{{text}}</div>
-      </div>
-      <div class="ebook-bookmark-icon-wrapper">
-        <bookmark :color="'red'"></bookmark>
-      </div>
+        <span class="iconfont icon-down"></span></div>
+      <div class="ebook-bookmark-text">{{text}}</div>
+    </div>
+    <div class="ebook-bookmark-icon-wrapper">
+      <bookmark :color="color" :width="15" :height="35"></bookmark>
     </div>
   </div>
 </template>
 
 <script>
+import {
+  ebookMixin
+} from '../../utils/mixin'
+import {
+  realPx
+} from '../../utils/utils'
 import Bookmark from '../common/BookMark'
+const BLUE = '#346cbc'
+const WHITE = '#fff'
 export default {
+  mixins: [ebookMixin],
+  computed: {
+    height() {
+      return realPx(35)
+    },
+    threshold() {
+      return realPx(55)
+    }
+  },
+  watch: {
+    offsetY(v) {
+      if (v >= this.height && v <= this.threshold) {
+        this.$refs.bookmark.style.top = `${-v}px`
+        this.text = this.$t('book.pulldownAddMark')
+        this.color = WHITE
+      } else if (v >= this.threshold) {
+        this.$refs.bookmark.style.top = `${-v}px`
+        this.text = this.$t('book.releaseAddMark')
+        this.color = BLUE
+      }
+    }
+  },
   components: {
     Bookmark
   },
   data() {
     return {
-      text: this.$t('book.pulldownAddMark')
+      text: '',
+      color: WHITE
     }
   }
 }
@@ -49,8 +79,18 @@ export default {
       transition: all .2s linear;
       @include center;
     }
+
+    .ebook-bookmark-text {
+      font-size: px2rem(16);
+      color: white;
+    }
   }
 
-  .ebook-bookmark-icon-wrapper {}
+  .ebook-bookmark-icon-wrapper {
+    position: absolute;
+    right: 0;
+    bottom: 0;
+    margin-right: px2rem(15);
+  }
 }
 </style>
