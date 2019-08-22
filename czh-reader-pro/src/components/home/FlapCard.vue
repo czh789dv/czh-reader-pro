0758d7284faafc29f792495a40832476f5fe8208
czh789dv <49721043+czh789dv@users.noreply.github.com>
2019-08-22 15:59:54 +0800

    随机弹出卡片

--- czh-reader-pro/src/components/home/FlapCard.vue
+++ czh-reader-pro/src/components/home/FlapCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flap-card-wrapper" v-show="flapCardVisible">
     <!-- 弹出动画 -->
-    <div class="flap-card-background" :class="{'animation': runFlapCardAnimation}">
+    <div class="flap-card-background" :class="{'animation': runFlapCardAnimation}" v-show="runFlapCardAnimation">
       <div class="flap-card" v-for="(item, index) in flapCardList" :key="index" :style="{zIndex: item.zIndex}">
         <div class="flap-card-circle">
           <div class="flap-card-semi-circle flap-card-semi-circle-left" :style="semiCircleStyle(item, 'left')" ref="left"></div>
@@ -13,16 +13,18 @@
       </div>
     </div>
     <!-- 随机推荐书籍 -->
-    <div class="book-card" :class="{'animation': runBookCardAnimation}">
+    <div class="book-card" :class="{'animation': runBookCardAnimation}" v-show="runBookCardAnimation">
       <!-- 图书弹出框整体 -->
       <div class="book-card-wrapper">
         <!-- 图片 -->
-        <div class="img-wrapper"></div>
+        <div class="img-wrapper">
+          <img class="img" :src=" data ? data.cover : ''">
+        </div>
         <!-- 文字信息 -->
         <div class="content-wrapper">
-          <div class="content-title">12312</div>
-          <div class="content-author">aaaa</div>
-          <div class="content-category">asdas</div>
+          <div class="content-title">{{data ? data.title : ''}}</div>
+          <div class="content-author">{{data ? data.author : ''}}</div>
+          <div class="content-category">{{categoryText()}}</div>
         </div>
         <!-- 按钮  点击跳转图书页 stop修饰符  -->
         <div class="read-btn" @click.stop="showBookDetail(data)">{{$t('home.readNow')}}</div>
@@ -37,13 +39,23 @@
 
 <script>
 import {
-  FLAP_CARD_LIST
+  FLAP_CARD_LIST,
+  categoryText
 } from '../../utils/store'
 import {
   homeMixin
 } from '../../utils/mixin'
 export default {
   methods: {
+    //获取章节文字
+    categoryText() {
+      if (this.data) {
+        categoryText(this.data.category, this)
+      } else {
+        return ''
+      }
+    },
+    //转动卡片
     rotate(index, type) {
       const item = this.flapCardList[index]
       let dom
@@ -56,22 +68,24 @@ export default {
       dom.style.transform = `rotateY(${item.rotateDegree}deg)`
       dom.style.backgroundColor = `rgb(${item.r}, ${item._g}, ${item.b})`
     },
-    //转动卡片
+    //开始转动动画
     startFlapCardAnimation() {
       // this.setFlapCardVisible(true)
       this.prepare()
       this.task = setInterval(() => {
         this.flapCardRotate()
       }, this.IntervalTime)
-      setTimeout(() => {
-        this.StopAnimation()
-      }, 2500)
     },
     //关闭动画相关
     StopAnimation() {
-      this.runFlapCardAnimation = false
       if (this.task) {
         clearInterval(this.task)
+      }
+      if (this.timeout) {
+        clearTimeout(this.timeout)
+      }
+      if (this.timeout2) {
+        clearTimeout(this.timeout2)
       }
       this.reset()
     },
@@ -86,6 +100,9 @@ export default {
         this.rotate(index, 'front')
         this.rotate(index, 'back')
       })
+      this.runFlapCardAnimation = false
+      this.runPointAnimation = false
+      this.runBookCardAnimation = false
     },
     //处理背面动画效果
     prepare() {
@@ -116,6 +133,7 @@ export default {
         this.next()
       }
     },
+    //处理下一张卡片
     next() {
       const frontFlapCard = this.flapCardList[this.front]
       const backFlapCard = this.flapCardList[this.back]
@@ -151,6 +169,7 @@ export default {
       })
       this.prepare()
     },
+    //动态设置卡片的样式
     semiCircleStyle(item, dir) {
       return {
         backgroundColor: `rgb(${item.r}, ${item.g}, ${item.b})`,
@@ -158,22 +177,29 @@ export default {
         backgroundImage: dir === 'left' ? item.imgLeft : item.imgRight
       }
     },
+    //关闭动画
     close() {
       this.setFlapCardVisible(false)
       this.StopAnimation()
     },
+    //开始小球动画
     startPonitAnimation() {
       this.runPointAnimation = true
       setTimeout(() => {
         this.runPointAnimation = false
       }, 1500)
     },
+    //开始全部动画
     runAnimation() {
       this.runFlapCardAnimation = true
-      setTimeout(() => {
+      this.timeout = setTimeout(() => {
         this.startFlapCardAnimation()
         this.startPonitAnimation()
       }, 300)
+      this.timeout2 = setTimeout(() => {
+        this.StopAnimation()
+        this.runBookCardAnimation = true
+      }, 2000)
     }
   },
   mixins: [homeMixin],
@@ -183,7 +209,7 @@ export default {
       flapCardList: FLAP_CARD_LIST,
       front: 0,
       back: 1,
-      IntervalTime: 10,
+      IntervalTime: 5,
       runFlapCardAnimation: false,
       pointList: null,
       runPointAnimation: false,
@@ -200,9 +226,9 @@ export default {
       this.pointList.push(`point${i}`)
     }
   },
-  // mounted() {
-  //   this.startFlapCardAnimation()
-  // },
+  mounted() {
+    console.log(this.data)
+  },
   //使用监听器启动
   watch: {
     flapCardVisible(v) {
@@ -322,17 +348,20 @@ export default {
   .book-card {
     position: relative;
     width: 65%;
+    max-width: px2rem(400);
+    box-sizing: border-box;
     border-radius: px2rem(15);
     background: white;
-    max-width: px2rem(300);
-    box-sizing: border-box;
-    &.animation{
+
+    &.animation {
       animation: scale .3s ease-in both;
+
       @keyframes scale {
         0% {
           transform: scale(0);
           opacity: 0;
         }
+
         100% {
           transform: scale(1);
           opacity: 1;
@@ -341,48 +370,61 @@ export default {
     }
 
     .book-card-wrapper {
+      margin-bottom: px2rem(30);
       width: 100%;
       height: 100%;
       //从上而下垂直居中布局
       @include columnTop;
+
       .img-wrapper {
         width: 100%;
         height: 100%;
+        margin-top: px2rem(20);
         @include center;
+
+        .img {
+          width: px2rem(90);
+          height: px2rem(130);
+        }
       }
 
       .content-wrapper {
+        padding: px2rem(10) px2rem(20);
+        margin-top: px2rem(10);
 
         .content-title {
           color: #333;
-          font-weight: blod;
-          line-height: px2rem(20);
+          font-weight: bold;
           font-size: px2rem(18);
+          line-height: px2rem(20);
+          max-height: px2rem(40);
           text-align: center;
           @include ellipsis1(2)
         }
 
         .content-author {
           margin-top: px2rem(10);
+          font-size: px2rem(12);
+          line-height: px2rem(14);
           text-align: center;
         }
 
         .content-category {
-          color:#999;
+          color: #999;
           font-size: px2rem(14);
           margin-top: px2rem(10);
           text-align: center;
-
         }
       }
 
       .read-btn {
-        position: relative;
+        position: absolute;
         bottom: 0;
         left: 0;
-        z-index: 1500;
+        z-index: 1100;
         width: 100%;
-        border-radius: px2rem(10);
+        border-radius: 0 0 px2rem(15) px2rem(15);
+        padding: px2rem(15) 0;
         text-align: center;
         color: white;
         font-size: px2rem(14);
