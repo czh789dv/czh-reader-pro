9a858ed051a156e62c738d02d74ac9da09406893
czh789dv <49721043+czh789dv@users.noreply.github.com>
2019-08-25 15:49:48 +0800

    书城首页布局

--- czh-reader-pro/src/components/home/CategoryBook.vue
+++ czh-reader-pro/src/components/home/CategoryBook.vue
@@ -1,12 +1,39 @@
 <template>
-  <div class="page">
+  <!-- 根据分类 显示分类里的所有书籍 -->
+  <div class="category-book">
+    <title1 :label="categoryText(data.category)" :btn="$t('home.seeAll')"></title1>
+    <div class="category-book-list">
+      <div class="category-book-item" v-for="(item, index) in data.list" :key="index">
+        <div class="img-wrapper">
+          <img :src="item.cover" class="img">
 
+        </div>
+        <div class="content-wrapper">
+          <div class="title">{{item.title}}</div>
+          <div class="author">{{item.author}}</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import {
+  categoryText
+} from '../../utils/store'
+import title1 from './Title'
 export default {
-  components: {},
+  methods: {
+    categoryText(category) {
+      return categoryText(category, this)
+    }
+  },
+  props: {
+    data: Object
+  },
+  components: {
+    title1
+  },
   data() {
     return {
 
@@ -15,6 +42,45 @@ export default {
 }
 </script>
 
-<style scoped lang="scss" rel="stylesheet/scss">
+<style lang="scss" scoped>
 @import "../../assets/styles/global";
+
+.category-book {
+  height: 100%;
+
+  .category-book-list {
+    width: 100%;
+    @include top;
+    padding: px2rem(5);
+    box-sizing: border-box;
+
+    .category-book-item {
+      flex: 0 0 25%;
+      width: 25%;
+      padding: px2rem(5);
+      box-sizing: border-box;
+
+      .img-wrapper {
+        @include center;
+        .img {
+          width: 100%;
+        }
+      }
+
+      .content-wrapper {
+        font-size: px2rem(12);
+        .title {
+          font-size: px2rem(14);
+          margin-top: px2rem(5);
+          @include ellipsis1(2);
+        }
+
+        .author {
+          margin-top: px2rem(5);
+          @include ellipsis1(2);
+        }
+      }
+    }
+  }
+}
 </style>
